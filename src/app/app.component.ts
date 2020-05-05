62cef4e0ae4716f6d493075fd129a52fcd46265b
Marcos Latchuk <marcos_latchuk@hotmail.com>
2020-05-05 16:48:48 -0300

    login implementado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { finalize } from 'rxjs/operators';
@@ -15,9 +16,11 @@ export class AppComponent {
   items: Observable<any[]>;
   uploadPercent: Observable<number>;
   downloadURL: Observable<string>;
+  usuario: Observable<firebase.User>;
 
-  constructor(firestore: AngularFirestore, private storage: AngularFireStorage){
+  constructor(private firestore: AngularFirestore, private storage: AngularFireStorage, public auth: AngularFireAuth){
     this.items = firestore.collection('items').valueChanges();
+    this.usuario = this.auth.user;
   }
   
   uploadFile(event) {
@@ -35,6 +38,22 @@ export class AppComponent {
      )
     .subscribe();
 
+  }
+
+  async addItem() {
+    await this.firestore.collection('items').add({description: 'testing another one'});
+  }
+
+  async cadastrar() {
+    await this.auth.createUserWithEmailAndPassword('marcoslatchuk@gmail.com', '123456');
+  }
+
+  async autenticar() {
+    await this.auth.signInWithEmailAndPassword('marcoslatchuk@gmail.com', '123456');
+  }
+
+  async sair() {
+    await this.auth.signOut();
   }
 
 }
